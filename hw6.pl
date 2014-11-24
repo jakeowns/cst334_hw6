a289ebb9fe26ce8f877daabf3d36e557bb11c024
Jacob Salinas <beanshield@gmail.com>
2014-11-23 21:31:11 -0800

    Change structure and refactor.

--- hw6.pl
+++ hw6.pl
@@ -1,29 +1,45 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use List::Util qw(sum);
-my (@u_burst, @burst, $s_time, @sum, @wait_time);
-# get burst time
-for(glob("/home/cst334/HW6/Job[0-9]*")){
-	$s_time = time();
-	system($_ . " >/dev/null 2>&1");
-	push @u_burst, (time() - $s_time);
-}
-print "Burst times: " . join(' ', @u_burst) . "\n";
-# sort ascending order
-@burst = sort { $a <=> $b } @u_burst;
-print "Burst times sorted: " . join(' ', @burst) . "\n";
-unshift @burst, 0;
-@wait_time = map {
-	if($_ == 0) {
-		$burst[$_];
-	} else {
-		$burst[$_] += $burst[$_-1];
+use feature qw(say);
+my (@u_burst, @burst);
+sub calc_wait {
+	my $jobs = shift;
+	for (0 .. $#{$jobs}) {
+		if($_ == 0) {
+			$jobs->[$_]{wait} = 0;
+		} else {
+			$jobs->[$_]{wait} = $jobs->[$_-1]{burst} + $jobs->[$_-1]{wait};
+		}
 	}
-} 0 .. $#burst;
-# print wait time
-print "Wait times: " . join(' ', @wait_time) . "\n";
-# print avg wait
-@sum = @wait_time;
-pop @sum;
-print "Average: " . sum(@sum)/$#wait_time . "\n";
+}
+sub print_jobs {
+	my $jobs = shift;
+	my $sum = 0;
+	for(@{$jobs}) {
+		$sum += $_->{wait};
+		say $_->{name} . ' burst-time: ' . $_->{burst} . 's wait-time: ' . $_->{wait} . 's';
+	}
+	say "Average waiting is: " . $sum/@{$jobs};
+}
+# init burst
+for(glob("/home/cst334/HW6/Job[0-9]*")){
+	my $wait = undef;
+	my $s_time = time();
+	system($_ . " >/dev/null 2>&1");
+	s{.*/}{};
+	push @u_burst, { 
+		'name' => $_,
+		'burst' => (time() - $s_time)
+	};
+}
+# calc wait
+say "Before:";
+calc_wait(\@u_burst);
+print_jobs(\@u_burst);
+# sort and calc wait
+say "After:";
+@burst = sort { $a->{burst} <=> $b->{burst} } @u_burst;  # sort ascending order
+calc_wait(\@burst);
+print_jobs(\@burst);
+__END__
